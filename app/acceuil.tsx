482891011e3d269b4c33002f18d647690fedcaed
joflamme <joflamme@gmail.com>
2024-04-21 15:12:00 +0200

    add raider io logo

--- app/acceuil.tsx
+++ app/acceuil.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { faW } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
 import { shadowsIntoLight } from '@/utils/font';
+import raiderIo from '../public/logo_raiderIO_blanc.png';
 
 const Acceuil = () => {
   const handleScrollToPostuler = () => {
@@ -25,13 +27,13 @@ const Acceuil = () => {
         </div>
         <div className="text-white text-center flex flex-row gap-7 pt-2">
           <a href="https://www.facebook.com/groups/281196140039473" target="_blank">
-            <FontAwesomeIcon className="text-white" width={15} icon={faFacebookF} />
+            <FontAwesomeIcon className="text-white" width={25} icon={faFacebookF} />
           </a>
           <a href="https://discord.com/invite/Ue4wbVa" target="_blank">
-            <FontAwesomeIcon className="text-white" width={30} icon={faDiscord} />
+            <FontAwesomeIcon className="text-white" width={25} icon={faDiscord} />
           </a>
           <a href="https://raider.io/guilds/eu/elune/%C3%89dition%20Limit%C3%A9e" target="_blank">
-            <FontAwesomeIcon className="text-white" width={25} icon={faW} />
+            <Image src={raiderIo} className="text-white" width={25} alt="Logo raiderIo" />
           </a>
         </div>
       </div>
