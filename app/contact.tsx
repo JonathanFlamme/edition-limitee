145c5b67b72a6596a9e28e5880125ff8351db386
joflamme <joflamme@gmail.com>
2024-04-26 20:02:07 +0200

    add space and ':'

--- app/contact.tsx
+++ app/contact.tsx
@@ -4,10 +4,10 @@ import React from 'react';
 const Contact = () => {
   return (
     <div id="contact" className="flex flex-col items-center bg-black text-white gap-2 py-20">
-      <h1 className={`${jost.className} text-4xl font-bold mb-4`}>Nous joindre</h1>
+      <h1 className={`${jost.className} text-4xl font-bold mb-4`}>Nous joindre :</h1>
       <div className="text-2xl">
         <p>Tweetÿ : tweety#2358</p>
-        <p>Joflamme: Joflamme#2580</p>
+        <p>Joflamme : Joflamme#2580</p>
         <p>Isthrale : LalKédu02#2766</p>
       </div>
     </div>
