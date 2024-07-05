6aefb2611b3b8b1e871947ebe660515cf4edd451
joflamme <joflamme@gmail.com>
2024-07-06 01:27:40 +0200

    Fix toast logout

--- app/components/profile/Login.tsx
+++ app/components/profile/Login.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 
-import { useSession, signIn } from 'next-auth/react';
-import Profile from '../profile/Profile';
+import { useSession } from 'next-auth/react';
 import LoginBnet from './LoginBnet';
 import { jost } from '@/utils/font';
 import { toast } from 'sonner';
+import Profile from './Profile';
 
 export default function Login() {
   const { data: session, status } = useSession();
@@ -12,8 +12,11 @@ export default function Login() {
   useEffect(() => {
     status === 'authenticated' &&
       toast.success(`Bienvenue sur le site, ${session.character.name} ! Heureux de vous revoir.`);
-    status === 'unauthenticated' && toast.info('Vous venez de vous déconnecter. À bientôt !');
   }, [status]);
 
-  return <div className={`${jost.className}`}>{!session ? <LoginBnet /> : <Profile />}</div>;
+  return (
+    <div className={`${jost.className}`}>
+      {status === 'authenticated' ? <Profile /> : <LoginBnet />}
+    </div>
+  );
 }
--- app/components/profile/Logout.tsx
+++ app/components/profile/Logout.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { signOut } from 'next-auth/react';
+import { toast } from 'sonner';
 
 export default function Logout() {
+  const handleLogout = async () => {
+    signOut({ redirect: false });
+    toast.info('Vous venez de vous déconnecter. À bientôt !');
+  };
+
   return (
     <p
-      className=" cursor-pointer text-xl my-2 text-white hover:text-gray-300"
-      onClick={() => signOut()}
+      className="cursor-pointer text-xl my-2 text-white hover:text-gray-300"
+      onClick={handleLogout}
     >
       Se déconnecter
     </p>
