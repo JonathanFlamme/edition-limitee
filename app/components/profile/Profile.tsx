8785647ab00548fdd7b467b84fe930f7f3a6f246
joflamme <joflamme@gmail.com>
2024-07-06 19:24:49 +0200

    use NextUI for Avatar

--- app/components/profile/Profile.tsx
+++ app/components/profile/Profile.tsx
@@ -1,21 +1,15 @@
 import React, { useState } from 'react';
-import Image from 'next/image';
 
 import { useSession } from 'next-auth/react';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Logout from './Logout';
 import { Popper } from '@mui/material';
 import CharactersByRealm from './CharactersByRealm';
 import { jost } from '@/utils/font';
+import { User } from '@nextui-org/user';
 
 export default function Profile() {
   const { data: session } = useSession();
   const [anchorElSetting, setAnchorElSetting] = useState<HTMLParagraphElement | null>(null);
-
-  if (!session) {
-    return null;
-  }
 
   const handleOpenSetting = (event: React.MouseEvent<HTMLParagraphElement>) => {
     setAnchorElSetting(anchorElSetting ? null : event.currentTarget);
@@ -25,44 +19,36 @@ export default function Profile() {
 
   return (
     <div>
-      <div
-        className="cursor-pointer md:flex md:mt-4 hover:text-gray-300"
+      <User
         onClick={handleOpenSetting}
-      >
-        <Image
-          className="rounded-full border-4 border-gray-500 mr-2 mt-2  md:mb-2 md:mt-0"
-          src={session.character?.avatar ?? ''}
-          alt="avatar"
-          width={60}
-          height={60}
-        />
-        <div className="hidden cursor-pointer md:flex md:flex-col justify-center hover:text-gray-300">
-          <p>
-            {session.character?.name}
-            <FontAwesomeIcon
-              className="text-white md:mt-1 md:ml-1"
-              width={20}
-              icon={faChevronDown}
-            />
-          </p>
-          <p className="capitalize text-lg">{session.character?.realm}</p>
-        </div>
+        name={session?.character?.name}
+        description={session?.character?.realm}
+        avatarProps={{
+          src: session?.character?.avatar,
+          size: 'lg',
+          radius: 'lg',
+        }}
+        classNames={{
+          base: 'cursor-pointer text-xl mt-3 mr-1 md:mr-0 md:mt-4 hover:text-gray-300',
+          name: 'text-xl hidden md:block',
+          description: 'capitalize text-sm hidden md:block',
+        }}
+      />
 
-        {/* Popover for setting */}
-        <Popper
-          open={isPopoverOpenSetting}
-          anchorEl={anchorElSetting}
-          onClick={(e) => e.stopPropagation()}
-          placement="bottom"
+      {/* Popover for setting */}
+      <Popper
+        open={isPopoverOpenSetting}
+        anchorEl={anchorElSetting}
+        onClick={(e) => e.stopPropagation()}
+        placement="bottom"
+      >
+        <div
+          className={`${jost.className} border-gray-400 border-2 rounded-lg shadow-lg py-2 px-4 bg-gray-600 mt-3`}
         >
-          <div
-            className={`${jost.className} border-gray-400 border-2 rounded-lg shadow-lg py-2 px-4 bg-gray-600 mt-3`}
-          >
-            <CharactersByRealm />
-            <Logout />
-          </div>
-        </Popper>
-      </div>
+          <CharactersByRealm />
+          <Logout />
+        </div>
+      </Popper>
     </div>
   );
 }
