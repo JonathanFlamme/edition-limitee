66191c69c77f7a003751a7a1718f2d4a946f00f7
joflamme <joflamme@gmail.com>
2024-07-17 11:58:10 +0200

    CRUD Contact : GET

--- src/app/api/landing/contact/route.ts
+++ src/app/api/landing/contact/route.ts
@@ -1,8 +1,11 @@
-import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 import prisma from '@/src/lib/prisma';
 
 export async function GET() {
-  const contact = await prisma.contact.findMany();
-  return NextResponse.json({ contact });
+  const contact = await prisma.contact.findMany({
+    orderBy: {
+      order: 'asc',
+    },
+  });
+  return NextResponse.json(contact);
 }
---
+++ src/components/landing/contact/Contact.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ContactList from './ContactList';
+import { ContactType } from '@/@type/type';
+
+async function fetchContact(): Promise<ContactType[]> {
+  const baseUrl = process.env.BASE_URL || `https://${process.env.VERCEL_URL}`;
+  const res = await fetch(`${baseUrl}/api/landing/contacts`, { cache: 'no-store' });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch GET data');
+  }
+  return await res.json();
+}
+
+export default async function Contact() {
+  const contacts = await fetchContact();
+
+  return (
+    <div id="contact" className="bg-black">
+      <ContactList contacts={contacts} />
+    </div>
+  );
+}
