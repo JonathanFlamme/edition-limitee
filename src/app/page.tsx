4b65b08a64d2b6f7bca1c1d6c8107f5b1747bfca
joflamme <joflamme@gmail.com>
2024-08-03 18:24:25 +0200

    create home/data with GET

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,11 @@ import Search from '@/src/components/home/search/Search';
 import Image from 'next/image';
 import grunge_black from '@/public/grunge_black.webp';
 
-export const revalidate = 3600;
-export const fetchCache = 'default-no-store';
-
 async function fetchHome() {
   const baseUrl = process.env.BASE_URL || `https://${process.env.VERCEL_URL}`;
 
-  const res = await fetch(`${baseUrl}/api/home`, {
-    cache: 'no-store',
+  const res = await fetch(`${baseUrl}/api/home/data`, {
+    next: { revalidate: 60, tags: ['home'] },
   });
   if (!res.ok) {
     return { presentations: [], contacts: [], searches: [] };
