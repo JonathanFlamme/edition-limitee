7e97d0c93a8d17e15da70352ef03df6064f2638e
joflamme <joflamme@gmail.com>
2024-08-13 16:01:36 +0200

    improved loader roster

--- src/app/rosters/page.tsx
+++ src/app/rosters/page.tsx
@@ -7,8 +7,6 @@ import { LoaderIcon } from 'lucide-react';
 import { useState } from 'react';
 
 export default function Roster() {
-  const [isLoading, setIsLoader] = useState<boolean>(false);
-
   async function getRoster() {
     const promise = fetch('/api/rosters', {
       method: 'GET',
@@ -24,19 +22,18 @@ export default function Roster() {
   }
 
   const { data, isPending, error } = useQuery({ queryKey: ['rosters'], queryFn: getRoster });
-  if (isPending) return <div>Loading...</div>;
+  if (isPending)
+    return (
+      <div className="m-auto flex flex-col justify-center items-center text-stroke text-white text-2xl">
+        <LoaderIcon color="white" className="animate-spin w-32 h-32" />
+        <p>Chargement en cours...</p>
+      </div>
+    );
   if (error) return <div>Error loading rosters: {error.message}</div>;
 
   return (
     <div className={`${jost.className} page-container`}>
-      {isLoading ? (
-        <div className="m-auto flex flex-col justify-center items-center text-stroke text-white text-2xl">
-          <LoaderIcon color="white" className="animate-spin w-32 h-32" />
-          <p>Chargement en cours...</p>
-        </div>
-      ) : (
-        <DataTable columns={columns} data={data} />
-      )}
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
