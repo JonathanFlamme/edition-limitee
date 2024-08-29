17a4db4650f7e257b72739d5578df49f41428422
joflamme <joflamme@gmail.com>
2024-08-29 17:16:20 +0200

    Can to selected 'roster' of 'all members'

--- src/store/memberStore.ts
+++ src/store/memberStore.ts
@@ -3,12 +3,25 @@ import { create } from 'zustand';
 
 interface State {
   members: MemberType[];
+  roster: MemberType[];
+}
+
+interface Actions {
   setMembers: (members: State['members']) => void;
   setupdateMember: (memberId: string, updates: Partial<MemberType>) => void;
 }
-export const useMemberStore = create<State>((set) => ({
+
+export const useMemberStore = create<State & Actions>((set, get) => ({
   members: [],
-  setMembers: (members) => set({ members }),
+  roster: [],
+  setMembers: (members) =>
+    set({
+      members,
+      roster: members.filter(
+        (member) =>
+          member.rank === 0 || member.rank === 2 || (member.rank >= 4 && member.rank <= 6),
+      ),
+    }),
   setupdateMember: (memberId, updates) =>
     set((state) => ({
       members: state.members.map((member) =>
